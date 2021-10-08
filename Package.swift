8055f18fecd9fe9c972e284fb7fa7f0f06766ed3
Oscar De Moya <oscar.demoya@koombea.com>
2021-10-08 18:05:01 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,16 +8,18 @@ import PackageDescription
 let package = Package(
     name: "SKPhotoBrowser",
     platforms: [
-        .iOS(.v11)
+        .iOS(.v9)
     ],
     products: [
         .library(
             name: "SKPhotoBrowser",
             targets: ["SKPhotoBrowser"])
     ],
+    dependencies: [],
     targets: [
         .target(
             name: "SKPhotoBrowser",
             path: "SKPhotoBrowser")
-    ]
+    ],
+    swiftLanguageVersions: [.v5]
 )
